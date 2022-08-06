4ffb82b83789dee10426f03f15143f1ec4249cfe
finayv2 <75693103+finayv2@users.noreply.github.com>
2022-08-06 14:24:33 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,17 @@
+-- // Mods - Gives only mods that pin
+-- // Everyone - you know
+-- // {'U', 'fucker'} - gives only to those players
+
 return {
-
-
-
+	['BaenHohoho'] = {"Pin","rbxassetid://9064980787",{"DrPixel"}},
+	['vactap'] = {"Pin","rbxassetid://9499540588",{"Mishchano","thr0wlng"}},
+	['Spy_Evan'] = {"Pin","rbxassetid://9064980787",{"DrPixel"}},
+	['STAFFPIN'] = {"Pin","rbxassetid://9973128797", "Mods"},
+	['kodahh'] = {"Pin","rbxassetid://9505926781",{"Sn1pezRogueAlt24","Airnour"}},
+	['Baen'] = {"Pin","rbxassetid://10041611642",{"DrPixel"}},
+	['Castlerslolbanana'] = {"Pin","rbxassetid://6417858325","Everyone"},
+	['kenbestdev'] = {"Pin","rbxassetid://9963275645",{"DrPixel"}},	
+	['Summer2022Sun'] = {"Pin","rbxassetid://10109796052", "Everyone"},
+	['Tubers'] = {'Pin', 'rbxassetid://8851743008', {'DrPixel', 'b1asp', 'MagnumPlays_YT', 'RevTree', 'tiltadofps'}};
+	['5MilVisits'] = {"Pin","rbxassetid://10499402625", "Everyone"},
 }
